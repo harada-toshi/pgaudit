8f020b827771acf7f372b30c8b52d8a85ce7f6ee
Masahiko Sawada <sawada.mshk@gmail.com>
2017-02-07 14:20:39 +0900

    improve regression test for flitering application_name.

--- sql/application_name.sql
+++ sql/application_name.sql
@@ -11,9 +11,9 @@ SET application_name TO DEFAULT;
 SELECT * FROM appname1;
 
 -- Check if the ERROR class audit log is emitted only during
--- application_name is 'appname1'.
-SELECT col2 FROM appname2; -- error
+-- application_name is 'appname2'.
+SELECT err_col FROM appname2; -- error
 SET application_name TO 'appname2';
-SELECT col2 FROM appname2; -- error, must be logged
+SELECT err_col FROM appname2; -- error, must be logged
 SET application_name TO DEFAULT ;
-SELECT col2 FROM appname2; -- error
+SELECT err_col FROM appname2; -- error
